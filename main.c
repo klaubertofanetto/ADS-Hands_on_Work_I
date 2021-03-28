f0761c102457e42273573044fb0e2579f8e336bf
ktofanetto <klaubertofanetto@gmail.com>
2021-03-27 21:27:50 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFERSIZE 128
+
+int main()
+{
+
+    int opcao, numero;
+    char buf[BUFFERSIZE];
+
+    do {
+        printf("Selecione a operação que deseja realizar:\n");
+        printf("Para converter de decimal para hexadecimal, digite 1\n");
+        printf("Para converter de hexadecimal para decimal, digite 2\n");
+        printf("Para sair, digite 3\n");
+
+        scanf("%i", &opcao);
+
+        switch (opcao){
+            case 1:
+                printf("Digite o valor que quer converter para hexadecimal \n");
+                fgetc(stdin);
+                fgets(buf, BUFFERSIZE, stdin);
+                if (sscanf(buf, "%d", &numero) != 1)
+                    printf("O valor informado não é um número decimal \n\n");
+                else
+                    printf("O número decimal %d em Hexadecimal é igual a %X \n\n", numero, numero);
+                break;
+            case 2:
+                printf("Digite o valor que quer converter para decimal \n");
+                fgetc(stdin);
+                fgets(buf, BUFFERSIZE, stdin);
+                if (sscanf(buf, "%X", &numero) != 1)
+                    printf("O valor informado não é um número hexadecimal \n\n");
+                else
+                    printf("O número hexadecimal %X em decimal é igual a %d \n\n", numero, numero);
+                break;
+            case 3:
+                printf("Volte sempre!");
+                break;
+            default:
+                printf("Opção inválida.\n\n");
+        }
+    } while (opcao != 3);
+}
+
